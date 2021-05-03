9ebf23bfd3c5782c25fff512143c59019cb15636
Manish8798 <mankum08071998@gmail.com>
2021-05-04 03:26:15 +0530

    ButterKnife replaced with ViewBinding

--- app/src/main/java/com/example/butterknifetoviewbindingexample/MainActivity.java
+++ app/src/main/java/com/example/butterknifetoviewbindingexample/MainActivity.java
@@ -3,32 +3,38 @@ package com.example.butterknifetoviewbindingexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import com.example.butterknifetoviewbindingexample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.image_view)
-    ImageView imageView;
+    //first let's run the app
 
-    @BindView(R.id.textView)
-    TextView textView;
+    //Now start the migration
+
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        textView.setText("Sample Text ButterKnife");
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.textView.setText("Sample Text ViewBinding");
+        binding.btnChange.setOnClickListener(v -> btnClick());
+        binding.imageView.setOnClickListener(v -> imgClick());
     }
 
-    @OnClick(R.id.btn_change)
     void btnClick() {
         Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public void imgClick() {
+        Toast.makeText(this, "Image Clicked", Toast.LENGTH_SHORT).show();
     }
 }
